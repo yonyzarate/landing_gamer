832f59b10b0bdde787f2fcf5fc128a0a8b9f84c1
Yony Zarate Paco <38514096+yonyzarate@users.noreply.github.com>
2026-07-05 02:25:05 -0400

    fead: agregar javacript al buscardor y a la marquesina

---
+++ assets/js/main.js
@@ -0,0 +1,18 @@
+document.addEventListener("DOMContentLoaded", (event) => {
+  let input_search = document.querySelector(".search__input");
+  let btn_search = document.querySelector(".navbar__button");
+
+  btn_search.addEventListener("click", () => {
+    let visible = document.querySelector(".visible");
+
+    if (visible) {
+      input_search.style.width = "0";
+      input_search.style.padding = "0";
+      input_search.classList.remove("visible");
+    } else {
+      input_search.style.width = "15rem";
+      input_search.style.padding = "0.5rem 0 0.5rem 0.5rem";
+      input_search.classList.add("visible");
+    }
+  });
+});
